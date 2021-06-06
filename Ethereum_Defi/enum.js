79ed87cf7cd67a282274bd6de1967694bada6138
doga ozcan <dogasava@gmail.com>
2021-06-06 16:36:08 +0300

    Update enum.js

--- Ethereum_Defi/enum.js
+++ Ethereum_Defi/enum.js
@@ -1,8 +1,8 @@
 const requestTypeError = {
    
-    account_create  : "account yaratılırken hata oluştu.",
-    contract_deploy : "sözleşme deploy edilirken hata oluştu.",
-    identity        : "blockchain üzerine veri gönderilirken hata oluştu.",
+    AddressSituation  : "AddressSituation error.",
+    Lock : "Lock error.",
+    ClaimInformation        : "ClaimInformation error.",
     identity_transactional : "blockchain üzerine veri gönderirken transaction seviyesinde hata oluştu.",
     identity_transactional_hash : "blockchain üzerine veri gönderirken transaction seviyesinde hash hatası oluştu.",
     hash_get        : "blockchain üzerinden veri getirilirken hata oluştu."
