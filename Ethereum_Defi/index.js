93fa5c91b2f0bb4d30c2e2b87274b7a006f6b45f
doga ozcan <dogasava@gmail.com>
2021-07-06 22:38:34 +0300

    Update index.js

--- Ethereum_Defi/index.js
+++ Ethereum_Defi/index.js
@@ -33,26 +33,14 @@ var value ;
     next();
   });
   */
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', '*');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Accept-Encoding,X-CSRF-Token,Authorization,accept,origin,X-Requested-With');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
- 
-
-
-    // Pass to next layer of middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
+};
+
+app.use(allowCrossDomain);
 
 /*Account yaratmak için rest api url
 *Çağırım : http://ip:port/AddressSituation
