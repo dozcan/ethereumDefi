f3a7928647bd08756b72fa83b32fae4153c83b82
doga ozcan <dogasava@gmail.com>
2021-07-06 19:20:25 +0300

    Update index.js

--- Ethereum_Defi/index.js
+++ Ethereum_Defi/index.js
@@ -13,8 +13,10 @@ const cors = require('cors');
 var express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit:1024*1024*1024,type:'application/json'}));
-
+//app.use(bodyParser.json({limit:1024*1024*1024,type:'application/json'}));
+app.use(cors({
+  credentials: true,
+}));
 let errorMessage;
 let errorCode;
 var rawResponseObject;
