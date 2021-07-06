2aba47930e82e183d6cdaa4b431bbc90f280dc15
doga ozcan <dogasava@gmail.com>
2021-07-06 18:30:29 +0300

    Update index.js

--- Ethereum_Defi/index.js
+++ Ethereum_Defi/index.js
@@ -16,6 +16,7 @@ var express = require('express');
 var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
