8bee06131c8b8efb5ede4a95ff0b1cc130a67b50
doga ozcan <dogasava@gmail.com>
2021-07-06 22:22:47 +0300

    Update index.js

--- Ethereum_Defi/index.js
+++ Ethereum_Defi/index.js
@@ -39,14 +39,16 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', '*');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Accept-Encoding,X-CSRF-Token,Authorization,accept,origin,X-Requested-With');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+
 
     // Pass to next layer of middleware
     next();
